846c40d20734dfbc94297d36559137a1800815a2
Nate <natemausert@gmail.com>
2022-08-17 20:55:34 -0700

    Start filling out homepage using chakra-ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
   return(
-    <div>Yo Yo</div>
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Heya, I'm a full stack developer and I'm trying my best.
+      </Box>
+      <Box display={{md:"flex"}}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nathan Mausert
+          </Heading>
+          <p>Coffee Gremlin ( Nicolas Cage Enthusaiast / Developer / Ex-Welder )</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
