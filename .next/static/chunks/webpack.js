c316d729ca7b6d96a2b5b0683036755b8c4e1abc
Nate <natemausert@gmail.com>
2022-08-17 20:47:28 -0700

    add layout for app main and index

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc79f6c7a2f24790"; }
+/******/ 		__webpack_require__.h = function() { return "7edcebea647e718a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,6 +1222,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
---
+++ components/layouts/main.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { Box, Container } from "@chakra-ui/react";
+
+const Main = ({children, router}) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viweport" content="width=device-width, initial-scale=1" />
+        <title> Nathan Mausert - Home Page</title>
+      </Head>
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
